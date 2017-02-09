bc46465441f17bddcc9142e9a46d69f32f7ce1af
Prasad8897 <codekarenge@gmail.com>
2017-02-09 10:26:17 +0530

    trying locally

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+//Change the text of the main-text
+
+var element = document.getElementById('main-text');
+element.innerHTML="new value";
+
+//Move image
+var pok = document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+	marginleft=marginleft+10;
+	img.style.marginLeft = marginleft+'px';
+};
+pok.onClick = function(){
+	var interval = setInterval(moveRight,100);
+};
